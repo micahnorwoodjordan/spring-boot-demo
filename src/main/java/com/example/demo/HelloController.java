abec770acb098b6c05724f40e856d2a65f07a91a
Micah Norwood <micahnorwoodjordan@gmail.com>
2025-02-13 22:53:38 -0700

    use Controller instead of RestController to dynamically serve templates

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,14 +1,29 @@
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
 
-@RestController
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import org.springframework.stereotype.Controller;
+
+// utilizing the `Controller` from the stereotype package to dynamically serve templates
+// import org.springframework.web.bind.annotation.RestController;
+
+
+
+@Controller
 public class HelloController {
 
 	@GetMapping("/")
 	public String index() {
 		return "Greetings from Spring Boot!";
+	}
+
+	@GetMapping("/greeting")
+	public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
+		model.addAttribute("name", name);
+		return "greeting";
 	}
 
 }
